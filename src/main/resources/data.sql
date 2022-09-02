1a4903309838021f17d302fc54ddea928d88dead
Solo <shihundusolomon@gmail.com>
2022-09-02 16:21:18 +0300

    in memory db works perfectly

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO students (id, first_name,last_name,email) VALUES (1, 'solo','mon','solo@gmail.com');
+INSERT INTO students (id, first_name,last_name,email) VALUES (2, 'james','colly','jc@gmail.com');
+INSERT INTO students (id, first_name,last_name,email) VALUES (3, 'rample','stilskin','rample@gmail.com');
