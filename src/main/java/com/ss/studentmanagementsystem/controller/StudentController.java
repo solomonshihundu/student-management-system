075ed3faf7afee6a6803eabd1de8add5a8ea16a0
Solo <shihundusolomon@gmail.com>
2022-09-02 16:04:02 +0300

    using in memory db instead

--- src/main/java/com/ss/studentmanagementsystem/controller/StudentController.java
+++ src/main/java/com/ss/studentmanagementsystem/controller/StudentController.java
@@ -1,12 +1,14 @@
 package com.ss.studentmanagementsystem.controller;
 
 import com.ss.studentmanagementsystem.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class StudentController {
+
 
     private StudentService studentService;
 
