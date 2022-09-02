c84a992a9561350f4fa88cc45a8730e58e350f37
Solo <shihundusolomon@gmail.com>
2022-09-02 18:34:31 +0300

    connected to mysql db successfully

--- src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO students (id, first_name,last_name,email) VALUES (1, 'solo','mon','solo@gmail.com');
-INSERT INTO students (id, first_name,last_name,email) VALUES (2, 'james','colly','jc@gmail.com');
-INSERT INTO students (id, first_name,last_name,email) VALUES (3, 'rample','stilskin','rample@gmail.com');
