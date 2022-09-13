12de12a5639a104bdc7ae406f2a6ef3b512e7296
Solo <shihundusolomon@gmail.com>
2022-09-13 14:58:28 +0300

    can add new students to db

--- src/main/java/com/ss/studentmanagementsystem/controller/StudentController.java
+++ src/main/java/com/ss/studentmanagementsystem/controller/StudentController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class StudentController {
@@ -29,7 +31,13 @@ public class StudentController {
     public String createStudentForm(Model model){
         Student student = new Student();
         model.addAttribute("student",student);
-        return "student";
+        return "create_student";
+    }
+
+    @PostMapping("/students")
+    public String saveStudent(@ModelAttribute("students") Student student){
+        studentService.saveStudent(student);
+        return "redirect:/students";
     }
 
 }
